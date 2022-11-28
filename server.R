382f9f621a6bac2e93c63b6e2c235f90eb8b53c4
KAUMELON <meloncoa@gmail.com>
2022-11-28 14:50:47 +0530

    added basic files

---
+++ server.R
@@ -0,0 +1,470 @@
+
+######## List of required packages for the tool
+
+library(ggplot2)
+library(GGally)
+library(CCA)
+library(CCP)
+library(Hmisc)
+
+############################################### server
+server <- function(input, output, session) {
+  # file uploading
+  csvfile <- reactive({
+    csvfile <- input$file1
+    if (is.null(csvfile)) {
+      return()
+    }
+
+    # Read the uploaded file
+    dt <- read.csv(csvfile$datapath,
+      header = input$header,
+      sep = ","
+    )
+    dt
+  })
+
+  # After reading the file the variables appear
+  output$var <- renderUI({
+    if (is.null(input$file1$datapath)) {
+      return()
+    }
+    colnames <- reactive({
+      names(csvfile())
+    })
+
+    observeEvent(csvfile(), {
+      updateCheckboxGroupInput(session, "X",
+        choices = colnames(),
+        selected = FALSE
+      )
+    })
+
+    inputVar <- reactive({
+      Fixedvar <- input$X
+
+      if (setequal(colnames(), Fixedvar)) {
+        # If sets are equal, return an empty string
+        return("")
+      } else {
+        # Return elements that are only in colnames
+        setdiff(colnames(), Fixedvar)
+      }
+    })
+
+    observeEvent(inputVar(), {
+      updateCheckboxGroupInput(session, "Y", choices = inputVar())
+    })
+    list(
+      # first checker box input
+      checkboxGroupInput(
+        "X",
+        "Please select the first set of variables (X)",
+        choices = names(csvfile()) # Choices should be the column names
+      ),
+      # second checker box input
+      checkboxGroupInput(
+        "Y",
+        "Please select the second set of variables (Y)",
+        choices = names(csvfile()) # Choices should be the column names
+      ),
+      actionBttn(
+        inputId = "submit",
+        label = "Run Analysis!",
+        color = "danger",
+        style = "jelly"
+      )
+    )
+  })
+
+  ############################### This note appear on opening
+  output$note1 <- renderUI({
+    if (is.null(input$file1$datapath)) {
+      return(HTML(
+        paste0(
+          " <h4> To perform analysis using your own dataset, prepare excel file in csv format by reading instruction below  </h4>
+<p>
+<ui>
+<li>Open a new blank excel file</li>
+<li>Values of variables should be entered as columns </li>
+<li>Variable names should be given as column headings </li>
+<li>Don't type or delete anything on other cells without data</li>
+<li>You can use any names for your columns. No space is allowed in the Column name. If space is required use underscore ‘_’ or ‘.’ full stop; for example ‘Variable name’ should be written as Variable_name or Variable.name</li>
+<li>Short names may be selected for column name as it may look good in path diagram</li>
+<li>Data should be arranged towards upper left corner and row above the data should not be left blank </li>
+<li>Type 'NA' in the cell with no observation</li>
+<li>Don't type and delete anything on other cells without data. If so select those cells, right click and click clear contents </li>
+<li>Give names to all column, Don't add any unnecessary columns that is not required for analysis</li>
+<li>Once all these are done, your file is ready. Now save it as CSV file. </li>
+<li><b>Upload file by clicking browse in the app </b></li>
+</ui>
+</p>
+<h5> You can download a model data set from below and test the App  </h5>
+"
+        )
+      ))
+    } else {
+      return()
+    }
+  })
+
+  ############################## download Example data set
+  output$data_set <- renderUI({
+    if (is.null(input$file1$datapath)) {
+      list(
+        selectInput(
+          "filenames",
+          "Choose a dataset:",
+          list.files(pattern = ".csv")
+        ),
+        downloadButton("downloadData", label = "Download csv file to test", class = "butt1")
+      )
+    } else {
+      return()
+    }
+  })
+
+  datasetInput <- reactive({
+    switch(input$filenames,
+      filenames
+    )
+  })
+
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      input$filenames
+    },
+    content = function(file) {
+      write.csv(
+        read.csv
+        (input$filenames, header = TRUE, sep = ","),
+        file,
+        row.names = FALSE
+      )
+    }
+  )
+  ######################### end data set download
+
+
+  ####################################################### Details about model dataset
+  output$note2 <- renderUI({
+    if (is.null(input$file1$datapath)) {
+      return(HTML(
+        paste0(
+          "
+<ui>
+<li>Following are the details about model datasets</li>
+<li><b>mmreg.csv</b>: details will be given here</li>
+</ui>
+</p>
+
+"
+        )
+      ))
+    } else {
+      return()
+    }
+  })
+  ###########################
+
+  ############## ANALYSIS BEGINS
+
+
+  # correlation in the dataset
+  output$corrmat1 <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        x <- csvfile()
+        cormat <- Hmisc::rcorr(as.matrix(x))
+        R <- round(cormat$r, 3)
+        p <- cormat$P
+        ## Define notions for significance levels; spacing is important.
+        mystars <- ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    ")))
+        Rnew <- matrix(paste(R, mystars, sep = ""), ncol = ncol(x))
+        diag(Rnew) <- paste(diag(R), " ", sep = "")
+        row.names(Rnew) <- names(x)
+        colnames(Rnew) <- names(x)
+        Rnew
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Pearson correlation between variables</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top")
+  )
+
+  ### Adding note under corrmat
+  output$note4corrmat <- renderUI({
+    if (is.null(input$file1$datapath)) {
+      return()
+    }
+    if (is.null(input$submit)) {
+      return()
+    }
+    if (input$submit > 0) {
+      HTML(paste0("<p>Significance Indicator: ***0.1%; **1%; *5% level</p>
+                    "))
+    }
+  })
+
+
+  ##  Testing significance using wilks lambda
+  output$wilks <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- dim(X)[1]
+        p <- length(X)
+        q <- length(Y)
+        wilk <- CCP::p.asym(rho, n, p, q, tstat = "Wilks")
+        wilk <- as.data.frame(wilk)
+        wilk <- wilk[, -1]
+        wilk
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Wilks' Lambda, using F-approximation (Rao's F)</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+  ##  Canonical Correlations
+  output$cancorr <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        rho_2 <- rho^2
+        cancorr <- cbind(rho, rho_2)
+        colnames(cancorr) <- c(
+          "Canonical Correlations",
+          "Squared Canonical Correlations"
+        )
+        cancorr
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Canonical Correlation Analysis</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+
+  ##  Raw canonical variables in set X
+  output$rawU <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        raw <- cc1[3:4]
+        raw1 <- raw$xcoef
+        raw1 <- as.data.frame(raw1)
+        colnames(raw1) <- paste("U", 1:n, sep = "")
+        raw1
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Raw Canonical Coefficients for the set X Variables</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+  ##  Raw canonical variables in set Y
+  output$rawV <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        raw <- cc1[3:4]
+        raw2 <- raw$ycoef
+        raw2 <- as.data.frame(raw2)
+        colnames(raw2) <- paste("V", 1:n, sep = "")
+        raw2
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Raw Canonical Coefficients for the set Y Variables</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+  ##  Correlation between X variables and U
+  output$load1 <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        cc2 <- CCA::comput(X, Y, cc1)
+        load1 <- cc2$corr.X.xscores
+        load1 <- as.data.frame(load1)
+        colnames(load1) <- paste("U", 1:n, sep = "")
+        load1
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Correlations Between X Variables and their Canonical Variables(U) (Loadings)</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+  ##  Correlation between Y variables and V
+  output$load2 <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        cc2 <- CCA::comput(X, Y, cc1)
+        load2 <- cc2$corr.Y.yscores
+        load2 <- as.data.frame(load2)
+        colnames(load2) <- paste("V", 1:n, sep = "")
+        load2
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Correlations Between Y Variables and their Canonical Variables (V) (Loadings)</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )
+
+  ##  Correlation between X variables and V
+  output$load3 <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        cc2 <- CCA::comput(X, Y, cc1)
+        load3 <- cc2$corr.X.yscores
+        load3 <- as.data.frame(load3)
+        colnames(load3) <- paste("V", 1:n, sep = "")
+        load3
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Correlations Between X Variables and the opposite group of canonical variates (V) (Loadings)</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )  
+  
+  
+  ##  Correlation between Y variables and U
+  output$load4 <- renderTable(
+    {
+      if (is.null(input$file1$datapath)) {
+        return()
+      }
+      if (is.null(input$submit)) {
+        return()
+      }
+      if (input$submit > 0) {
+        X <- as.data.frame(subset(csvfile(), select = input$X))
+        Y <- as.data.frame(subset(csvfile(), select = input$Y))
+        cc1 <- CCA::cc(X, Y)
+        rho <- cc1$cor
+        n <- length(rho)
+        cc2 <- CCA::comput(X, Y, cc1)
+        load4 <- cc2$corr.Y.xscores
+        load4 <- as.data.frame(load4)
+        colnames(load4) <- paste("U", 1:n, sep = "")
+        load4
+      }
+    },
+    rownames = TRUE,
+    bordered = TRUE,
+    align = "c",
+    caption = ("<b>Correlations Between Y Variables and the opposite group of canonical variates (U) (Loadings)</b>"),
+    caption.placement = getOption("xtable.caption.placement", "top"),
+    digits = 4
+  )  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  }
---
+++ ui.R
@@ -0,0 +1,107 @@
+
+######## List of required packages for appearance
+
+library(shiny)
+library(shinyWidgets)
+library(rmarkdown)
+library(shinycssloaders)
+
+################################################## Ui
+ui <- fluidPage(
+  # Background colour
+  setBackgroundColor(
+    color = c("#f5ffcf", "#ffffff"),
+    gradient = "radial",
+    direction = c("bottom", "right")
+  ),
+  # Title Panel and colour
+  titlePanel(tags$div(tags$b("Canonical Correlation"), style = "color:#000000")),
+
+  # Entries in sidebar panel
+
+  sidebarPanel(
+    fileInput("file1", "CSV File (upload in csv format)",
+      accept = c(
+        "text/csv",
+        "text/comma-separated-values,text/plain",
+        ".csv"
+      )
+    ),
+    checkboxInput("header", "Header", TRUE),
+    uiOutput("var"),
+    tags$br(),
+    h5(
+      tags$div(
+        "Developed by:",
+        tags$br(),
+        tags$b("Dr. Pratheesh P. Gopinath"),
+        tags$br(),
+        tags$b("Assistant Professor,"),
+        tags$br(),
+        tags$b("Agricultural Statistics,"),
+        tags$br(),
+        tags$b("Kerala Agricultural University"),
+        tags$br(),
+        tags$br(),
+        tags$br(),
+        tags$b("Sandra M.M."),
+        tags$br(),
+        tags$b("MSc Agricultural Statistics"),
+        tags$br(),
+        tags$b("Kerala Agricultural University"),
+        tags$br(),
+        tags$br(),
+        h3(),
+        "post your queries at: pratheesh.pg@kau.in",
+        style = "color:#343aeb"
+      )
+    ),
+    conditionalPanel(
+      "$('#path').hasClass('recalculating')",
+      tags$div(tags$b("Loading ...please wait while calculation is going on in the background.....please dont press submit button again "), style = "color:green")
+    )
+  ),
+  mainPanel(
+    tabsetPanel(
+      type = "tab",
+      tabPanel(
+        "Analysis.Results",
+        conditionalPanel(
+          "$('#path').hasClass('recalculating')",
+          tags$div(tags$b("Loading ...please wait while calculation is going on in the background.....please dont press submit button again "), style = "color:green")
+        ),
+        uiOutput("note1"),
+        tags$br(),
+        uiOutput("data_set"),
+        tags$br(),
+        uiOutput("note2"),
+        tableOutput("corrmat1"),
+        uiOutput("note4corrmat"),
+        tags$br(),
+        tableOutput("wilks"),
+        tags$br(),
+        tableOutput("cancorr"),
+        tags$br(),
+        tableOutput("rawU"),
+        tags$br(),
+        tableOutput("rawV"),
+        tags$br(),
+        tableOutput("load1"),
+        tags$br(),
+        tableOutput("load2"),
+        tags$br(),
+        tableOutput("load3"),
+        tags$br(),
+        tableOutput("load4"),
+        tags$br(),
+        tags$br(),
+        tags$br()
+      ),
+      tabPanel(
+        "Figures",
+        tags$br(),
+        tags$br()
+      )
+    )
+  )
+)
